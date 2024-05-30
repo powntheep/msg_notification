27c59b334ea07d4940658a15d9d1e045e7db07a4
n98365184 <165314898+n98365184@users.noreply.github.com>
2024-05-30 09:10:19 +0700

    deps: update

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,10 +7,8 @@ import Foundation
 
 import msg_notification
 import path_provider_foundation
-import sqflite
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   MsgNotificationPlugin.register(with: registry.registrar(forPlugin: "MsgNotificationPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
-  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
